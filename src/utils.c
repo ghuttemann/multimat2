43530955a3e67b6b061216a8ff6a10f9a8bdd36c
ghuttemann <ghuttemann@b9e49f7b-724d-0410-94b3-db543266da34>
2008-05-28 22:11:06 +0000

    - Se agrega la función para medir el tiempo. - Se cambia la función "trunc" por "(double) ((int) x)".

--- src/utils.c
+++ src/utils.c
@@ -4,7 +4,7 @@ void *xmalloc(size_t size) {
     void *ptr;
     
     if ((ptr = malloc(size)) == NULL)
-        LOG(FATAL, "%s: %s", __func__, "Memoria no disponible.");
+        LOG(FATAL, "%s(): %s", __func__, "Memoria no disponible.");
     
     return ptr;
 }
@@ -24,10 +24,29 @@ bool es_cuadrado_perfecto(int numero) {
 		return false;
 	
 	double raiz_decimal = sqrt(numero);
-	double raiz_entera  = trunc(raiz_decimal);
+	double raiz_entera  = (double) ((int) raiz_decimal);
 	
 	if (raiz_decimal == raiz_entera)
 		return true;
 	
 	return false;
+}
+
+long long get_time_millis(void) {
+	long long segundos, milisegundos;
+	struct timeval tv;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		LOG(FATAL, "%s(): %s", __func__, "Error al obtener el tiempo.");
+	
+	// Guardamos los tiempos en long long int
+	segundos     = tv.tv_sec;
+	milisegundos = tv.tv_usec;
+
+	// Convertimos los tiempos en milisegundos
+	segundos     = segundos * 1000;
+	milisegundos = milisegundos / 1000;
+
+	// Retornamos el resultado
+	return (segundos + milisegundos);
 }
