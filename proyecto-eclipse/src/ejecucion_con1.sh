e051e7fad968926f497ea50868ed35b87a95ff70
ghuttemann <ghuttemann@b9e49f7b-724d-0410-94b3-db543266da34>
2008-06-29 00:12:18 +0000

    Corrección en los scripts de ejecución para generación correcta de nombres de archivos de salida.

--- proyecto-eclipse/src/ejecucion_con1.sh
+++ proyecto-eclipse/src/ejecucion_con1.sh
@@ -2,12 +2,25 @@
 
 FILE=matrix-mult_tiempos
 
-for N in 200 400 800 1600; do
-	for H in 2 4 8 9 16; do
+for N in 400 800 1200 1600 2000 2400 2800 3200; do
+	for H in 1 2 4 8 9 16 32 36 64; do
 	echo "Ejecutando Concurrente $N 1d $H hilos"
 		for i in 1 2 3; do
 			./matrix-mult -a $N $N -b $N $N -h $H -t 1 -ni
-			mv $FILE.csv $FILE-con1-0$N-0$H-$i.csv
+			
+			if test $N -lt 1000 then
+                if test $H -lt 10 then
+                    mv $FILE.csv $FILE-con1-0$N-0$H-$i.csv
+                else
+                    mv $FILE.csv $FILE-con1-0$N-$H-$i.csv
+                fi
+			else
+    			if test $H -lt 10 then
+                    mv $FILE.csv $FILE-con1-$N-0$H-$i.csv
+                else
+                    mv $FILE.csv $FILE-con1-$N-$H-$i.csv
+                fi
+			fi
 		done
 	done
 done
--- proyecto-eclipse/src/ejecucion_sec.sh
+++ proyecto-eclipse/src/ejecucion_sec.sh
@@ -2,10 +2,15 @@
 
 FILE=matrix-mult_tiempos
 
-for N in 200 400 800 1600; do
+for N in 400 800 1200 1600 2000 2400 2800 3200; do
 	echo "Ejecutando Secuencial $N"
 	for i in 1 2 3; do
 		./matrix-mult -a $N $N -b $N $N -ni
-		mv $FILE.csv $FILE-sec0$N-$i.csv
+		
+        if test $N -lt 1000 then
+            mv $FILE.csv $FILE-sec0$N-$i.csv
+        else
+            mv $FILE.csv $FILE-sec$N-$i.csv
+        fi
 	done
 done
